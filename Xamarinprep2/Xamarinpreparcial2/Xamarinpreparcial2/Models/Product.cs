8d4614ecfdaa7480d04d3db7e75e97f100455e19
joel021198 <61084642+joel021198@users.noreply.github.com>
2020-06-03 20:51:44 -0400

    parte 1

---
+++ Xamarinprep2/Xamarinpreparcial2/Xamarinpreparcial2/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace Xamarinpreparcial2.Models
+{
+    using Newtonsoft.Json;
+
+    public class Product
+    {
+        [JsonProperty(PropertyName = "productid")]
+        public int ProductId { get; set; }
+
+
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+
+
+        [JsonProperty(PropertyName = "value")]
+        public double Value { get; set; }
+        
+
+    }
+}
